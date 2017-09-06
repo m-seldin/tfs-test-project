8f69c660b68ce99ecc98eb8a4eeb5cee6f3c5b81
Michael Seldin <michael.seldin@hpe.com>
2017-09-06 18:37:08 +0300

    unit testing

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ToolsLibrary;
 namespace UnitTestProject1
@@ -6,14 +7,27 @@ namespace UnitTestProject1
     [TestClass]
     public class TestHelperLibrary
     {
+        public TestContext TestContext { get; set; }
+
         HelperFunctions helperFunctions = new HelperFunctions();
         [TestMethod]
         public void TestConcat()
         {
+            
             var param1 = "a";
             var param2 = "b";
             var res = helperFunctions.MyConcat(param1, param2);
-            Assert.AreEqual(param1 + param2 ,res,$"Concat should be : {param1 + param2}") ;
+            
+
+            var shouldFail = TestContext?.Properties["shouldFail"];
+            Trace.WriteLine($"Should fail ? {shouldFail ?? "null"}");
+
+            if (shouldFail!=null && bool.Parse(shouldFail.ToString()) )
+                Assert.AreEqual(param1 + param2 ,res,$"Concat should be : {param1 + param2}") ;
+            else
+            {
+                Assert.AreEqual(param1 + param2, res, $"Concat should be : {param1 + param2}c");
+            }
         }
 
         public void TestPlus()
