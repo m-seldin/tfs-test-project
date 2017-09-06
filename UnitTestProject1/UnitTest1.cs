05e09f450a1a2887f4ab8e998767e81c6a9f89ca
Michael Seldin <michael.seldin@hpe.com>
2017-09-06 15:50:57 +0300

    add library and tests

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ToolsLibrary;
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class TestHelperLibrary
+    {
+        HelperFunctions helperFunctions = new HelperFunctions();
+        [TestMethod]
+        public void TestConcat()
+        {
+            var param1 = "a";
+            var param2 = "b";
+            var res = helperFunctions.MyConcat(param1, param2);
+            Assert.AreEqual(param1 + param2 ,res,$"Concat should be : {param1 + param2}") ;
+        }
+
+        public void TestPlus()
+        {
+            var param1 = 1;
+            var param2 = 2;
+            var res = helperFunctions.MyPlus(param1, param2);
+            Assert.AreEqual(param1 + param2, res, $"Concat should be : {param1 + param2}");
+        }
+    }
+}
