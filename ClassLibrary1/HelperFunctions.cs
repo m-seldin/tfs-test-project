dc5fa819063acfdebd56737ad26ef02f21f1d3bc
Michael Seldin <michael.seldin@hpe.com>
2017-12-04 15:59:38 +0200

    ADD NEW POROHJETC

---
+++ ClassLibrary1/HelperFunctions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToolsLibrary
+{
+    public class HelperFunctions
+    {
+        public string MyConcat(string a, string b)
+        {
+            return a + b + "c";
+        }
+
+        public int MyPlus(int a, int b)
+        {
+            return a + b;
+        }
+
+    }
+}
---
+++ UnitTestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("af93f19d-25a7-4f98-8427-7d63073cde71")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
