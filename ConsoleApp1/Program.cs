c35caf14d8b78470485e758ee5f4d405040251d0
Gitit Steingarten <gitit.steingarten@hpe.com>
2018-02-19 13:20:08 +0200

    Edit5

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            System.Console.WriteLine("Hello world");
+            System.Console.WriteLine("Hello world!");
         }
     }
 }
