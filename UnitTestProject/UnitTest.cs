cd74a2d786ba9c93f640157533109c1f77a83cba
Sarah Gabriel <sarah.gabriel@hpe.com>
2018-03-22 11:25:00 +0200

    Remove unitestproject1

---
+++ UnitTestProject/UnitTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ToolsLibrary;
+namespace UnitTestProject
+{
+    [TestClass]
+    public class TestHelperLibrary
+    {
+        public TestContext TestContext { get; set; }
+
+        HelperFunctions helperFunctions = new HelperFunctions();
+        [TestMethod]
+        public void TestConcat()
+        {
+            
+            var param1 = "a";
+            var param2 = "b";
+            var res = helperFunctions.MyConcat(param1, param2);
+
+            Assert.AreEqual(param1 + param1 + "c", res, "Concat not working!");
+
+        }
+        [TestMethod]
+        public void TestPlus()
+        {
+            var param1 = 1;
+            var param2 = 2;
+            var res = helperFunctions.MyPlus(param1, param2);
+            Assert.AreEqual(param1 + param2, res, "ERROR!");
+        }
+
+        [TestMethod]
+        public void TestMinus()
+        {
+            var param1 = 1;
+            var param2 = 2;
+            var res = helperFunctions.MyMinus(param1, param2);
+            Assert.AreEqual(param2 - param1, res, "ERROR!");
+        }
+    }
+}
